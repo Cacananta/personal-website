3575799a87e6cc001f87ccf43c3c3f0c2defd374
Corey Cacananta <cacananta@gmail.com>
2017-11-30 15:49:11 -0800

    media queries added

--- views/public/script.js
+++ views/public/script.js
@@ -1,35 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // Parallax Scrolling
-    var scale = 0.4;
-    function parallaxScroll() {
+  // Parallax Scrolling
+  var scale = 0.4;
+  function parallaxScroll() {
+    if ($(window).width() <= 1024) {
+      console.log("parallax: off")
+    } else {
       var scrollTop = $(window).scrollTop();
 
-      $('.parallax, .parallax2').each(function() {
+      $('.parallax, .parallax2').each(function () {
         $(this).css('top', Math.round(((0 - scrollTop) * scale)) + 'px');
       });
     }
+  }
 
-    // Smooth Scrolling
-    $(function() {
-      $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 285
-            }, 1500);
-            return false;
-          }
+  // Smooth Scrolling
+  $(function () {
+    $('a[href*=\\#]:not([href=\\#])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 285
+          }, 1500);
+          return false;
         }
-      });
+      }
     });
+  });
 
-    // Function Calls 
-    $(window).bind( 'scroll', function() {
-      parallaxScroll();
-    });
+  // Function Calls 
+  $(window).bind('scroll', function () {
     parallaxScroll();
+  });
+  parallaxScroll();
 
 });
